60feb32d0d51444a7060569b6ba29f05c8418c8b
Michael Riegert <michael@eowyn.net>
2021-05-12 01:05:28 -0400

    Start of basic assembler

--- README.md
+++ README.md
@@ -58,24 +58,24 @@ Caviates:
 
 | instr | operation | code | combin |
 |:-----:|:---------:|:----:|:------:|
-| `NOP`         | No operation                      | `000_XXXX_XXXX` |  256 |
-| `AND RR SS`   | And S and R, store in R           | `001_0000_RRSS` |   16 |
-| `OR  RR SS`   | Or S and R, store in R            | `001_0001_RRSS` |   16 |
-| `ADD RR SS`   | Add R to S, store in R            | `001_0010_RRSS` |   16 |
-| `SUB RR SS`   | Sub S from R, store in R          | `001_0011_RRSS` |   16 |
-| `INC RR`      | Increment R                       | `001_0100_00RR` |    4 |
-| `DEC RR`      | Decrement R                       | `001_0101_00RR` |    4 |
-| `CMP RR SS`   | Compare R with S                  | `001_1000_RRSS` |   16 |
-| `LDD RR DD`   | Load from data D into R           | `010_DDDD_00RR` |   64 |
-| `LDR RR SS`   | Load from D based on S into R     | `010_00SS_01RR` |   16 |
-| `STD RR DD`   | Store R in data D                 | `010_DDDD_10RR` |   64 |
-| `STR RR SS`   | Store R in addr by 4 bits of S    | `010_00SS_11RR` |   16 |
-| `JMP RR`      | Jump to address in R              | `011_0000_00RR` |    4 |
-| `JMP RR CC`   | Jump to address in R if C         | `011_CCCC_00RR` |   64 |
-| `LDI RR VV`   | Load immediate V into R           | `1RR_VVVV_VVVV` | 1024 |
-| ` `           |                                   | `001_011X_XXXX` |   32 |
-| ` `           |                                   | `001_1---_XXXX` |  128 |
-| ` `           |                                   | `011_----_XX--` |    4 |
+| `NOP`         | No operation                   | `000_XXXX_XXXX` |  256 |
+| `AND RR SS`   | And S and R, store in R        | `001_0000_RRSS` |   16 |
+| `OR  RR SS`   | Or S and R, store in R         | `001_0001_RRSS` |   16 |
+| `ADD RR SS`   | Add R to S, store in R         | `001_0010_RRSS` |   16 |
+| `SUB RR SS`   | Sub S from R, store in R       | `001_0011_RRSS` |   16 |
+| `INC RR`      | Increment R                    | `001_0100_00RR` |    4 |
+| `DEC RR`      | Decrement R                    | `001_0101_00RR` |    4 |
+| `CMP RR SS`   | Compare R with S               | `001_1000_RRSS` |   16 |
+| `LDD RR DD`   | Load from data D into R        | `010_DDDD_00RR` |   64 |
+| `LDR RR SS`   | Load from D based on S into R  | `010_00SS_01RR` |   16 |
+| `STD RR DD`   | Store R in data D              | `010_DDDD_10RR` |   64 |
+| `STR RR SS`   | Store R in addr by 4 bits of S | `010_00SS_11RR` |   16 |
+| `JMP RR`      | Jump to address in R           | `011_0000_00RR` |    4 |
+| `JMP RR CC`   | Jump to address in R if C      | `011_CCCC_00RR` |   64 |
+| `LDI RR VV`   | Load immediate V into R        | `1RR_VVVV_VVVV` | 1024 |
+| ` `           |                                | `001_011X_XXXX` |   32 |
+| ` `           |                                | `001_1---_XXXX` |  128 |
+| ` `           |                                | `011_----_XX--` |    4 |
 
 | CMP | CCCC | int |
 |:---:|:----:|:---:|
---
+++ ecccpu/ecccpu_asm.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+
+class EccCPU_ASM():
+    symbols = {}
+    code = []
+    asm = []
+
+    def run(self):
+        self.findSymbols()
+        self.clean()
+        print(f'symbols: {self.symbols}')
+        # print(f'code: {self.code}')
+        line_i = 0
+        for line in self.code:
+            for sym in self.symbols:
+                if line_i == self.symbols[sym]:
+                    print(f'                {sym}:')
+
+            op, arg1, arg2 = self.parse(line)
+            asm = self.encode(op, arg1, arg2)
+            print(f'{asm} {line_i:3}   {line}')
+            self.asm.append(asm)
+            line_i = line_i + 1
+
+    def findSymbols(self):
+        line_i = 0;
+        for line in self.code:
+            line = line.split(';')[0].strip()
+            if line == "":
+                pass
+            elif line.split(' ')[0][-1] == ":":
+                # print("Found symbol: {}={}".format(line[:-1], line_i))
+                self.symbols[line[:-1]] = line_i
+            else:
+                line_i = line_i + 1
+
+    def clean(self):
+        new_code = []
+        for line_i in range(len(self.code)):
+            line = self.code[line_i].split(';')[0].strip()
+            if line != "" and ":" not in line:
+                new_code.append(line)
+        self.code = new_code[:]
+
+    def parse(self, instr):
+        op = instr.split(' ')[0].lower()
+        arg1 = ""
+        arg2 = ""
+        if len(instr.split(' ')) > 1:
+            if instr.split(' ')[1].isnumeric():
+                arg1 = int(instr.split(' ')[1].lower())
+            elif instr.split(' ')[1].lower() in self.symbols:
+                arg1 = self.symbols[instr.split(' ')[1].lower()]
+            else:
+                print("Error: Unknown arg: {}".format(instr.split(' ')[1]))
+        if len(instr.split(' ')) > 2:
+            if instr.split(' ')[2].isnumeric():
+                arg2 = int(instr.split(' ')[2].lower())
+            elif instr.split(' ')[2].lower() in self.symbols:
+                arg2 = self.symbols[instr.split(' ')[2].lower()]
+            else:
+                print("Error: Unknown arg: {}".format(instr.split(' ')[2]))
+        # print("instr: {} {} {}".format(op, arg1, arg2))
+        return op, arg1, arg2
+
+    def encode(self, op, arg1, arg2):
+        asm = ''
+        if (op == "nop"):
+            asm = f'{0:011b}'
+        elif (op == "and"):
+            asm = f'0010000{arg1:02b}{arg2:02b}'
+        elif (op == "or"):
+            asm = f'0010001{arg1:02b}{arg2:02b}'
+        elif (op == "add"):
+            asm = f'0010010{arg1:02b}{arg2:02b}'
+        elif (op == "sub"):
+            asm = f'0010011{arg1:02b}{arg2:02b}'
+        elif (op == "inc"):
+            asm = f'001010000{arg1:02b}'
+        elif (op == "dec"):
+            asm = f'001010100{arg1:02b}'
+        elif (op == "cmp"):
+            asm = f'0011000{arg1:02b}{arg2:02b}'
+        elif (op == "ldd"):
+            asm = f'010{arg2:04b}00{arg1:02b}'
+        elif (op == "ldr"):
+            asm = f'01000{arg2:02b}01{arg1:02b}'
+        elif (op == "std"):
+            asm = f'010{arg2:04b}10{arg1:02b}'
+        elif (op == "str"):
+            asm = f'01000{arg2:02b}11{arg1:02b}'
+        elif (op == "jmp"):
+            if arg2 == "":
+                asm = f'011000000{arg1:02b}'
+            else:
+                asm = f'011{arg2:04b}00{arg1:02b}'
+        elif (op == "ldi"):
+            asm = f'1{arg1:02b}{arg2:08b}'
+        else:
+            print("Unknown instruction: {}".format(op))
+            exit(1)
+        return asm
+
+    def parity(self, data_in):
+        return 0
+
+if __name__=="__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', required=True)
+    parser.add_argument('-o', required=True)
+
+    args = parser.parse_args()
+
+    asm = EccCPU_ASM()
+    asm.code = open(args.i).read().replace('\r','').split('\n')
+    asm.run()
